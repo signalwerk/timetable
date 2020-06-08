42cb504b74e320051b864d2371ecddc649532356
signalwerk <sh@signalwerk.ch>
2020-06-08 13:23:06 +0200

    FIX: fragment

--- src/components/lession/index.js
+++ src/components/lession/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Tableview from '../tableview'
 import Anchor from '../anchor'
 import { TotalLessions } from './totalLessions'
